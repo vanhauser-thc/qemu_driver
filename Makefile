0f2d91433cbd176c4e388395de1a5b88b1a8081f
van Hauser <vh@thc.org>
2020-08-10 00:18:25 +0200

    make leaner

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ libQEMU.a:	qemu_driver.o
 	ar r libQEMU.a qemu_driver.o
 
 qemu_driver.o:	qemu_driver.cpp
-	clang++ -stdlib=libc++ -std=c++11 -O2 -c qemu_driver.cpp
+	$(CC) -O3 -c qemu_driver.cpp
 
 clean:
 	rm -f libQEMU.a qemu_driver.o core *~
--- qemu_driver.cpp
+++ qemu_driver.cpp
@@ -1,10 +1,3 @@
-//===- qemu_driver.cpp - a glue between a qemu fuzzer and libFuzzer --------*- C++ -* ===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//===----------------------------------------------------------------------===//
-
 /* This file allows to fuzz libFuzzer-style target functions
  (LLVMFuzzerTestOneInput) with a fuzzer's qemu mode.
 
@@ -24,18 +17,8 @@ EOF
 # Build this file, link it with afl-llvm-rt.o.o and the target code.
 clang++ qemu_driver.cpp test_fuzzer.o
 # Run AFL:
-rm -rf IN OUT; mkdir IN OUT; echo z > IN/z;
-$AFL_HOME/afl-fuzz -Q -i IN -o OUT ./a.out
-################################################################################
-AFL_DRIVER_STDERR_DUPLICATE_FILENAME: Setting this *appends* stderr to the file
-specified. If the file does not exist, it is created. This is useful for getting
-stack traces (when using ASAN for example) or original error messages on hard
-to reproduce bugs. Note that any content written to stderr will be written to
-this file instead of stderr's usual location.
-
-AFL_DRIVER_CLOSE_FD_MASK: Similar to libFuzzer's -close_fd_mask behavior option.
-If 1, close stdout at startup. If 2 close stderr; if 3 close both.
-
+$ mkdir in; echo > in/in;
+$ afl-fuzz -Q -i in -o out -- ./a.out
 */
 #include <assert.h>
 #include <errno.h>
@@ -50,7 +33,6 @@ If 1, close stdout at startup. If 2 close stderr; if 3 close both.
 #include <iostream>
 #include <vector>
 
-// Platform detection. Copied from FuzzerInternal.h
 #ifdef __linux__
   #define LIBFUZZER_LINUX 1
   #define LIBFUZZER_APPLE 0
@@ -86,23 +68,21 @@ If 1, close stdout at startup. If 2 close stderr; if 3 close both.
 #endif
 
 // libFuzzer interface is thin, so we don't include any libFuzzer headers.
-extern "C" {
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size);
 __attribute__((weak)) int LLVMFuzzerInitialize(int *argc, char ***argv);
-}
 
 // Notify AFL about persistent mode.
 static volatile char AFL_PERSISTENT[] = "##SIG_AFL_PERSISTENT##";
-extern "C" int __afl_persistent_loop(unsigned int);
+int __afl_persistent_loop(unsigned int);
 static volatile char suppress_warning2 = AFL_PERSISTENT[0];
 
 // Notify AFL about deferred forkserver.
 static volatile char AFL_DEFER_FORKSVR[] = "##SIG_AFL_DEFER_FORKSRV##";
-extern "C" void __afl_manual_init();
+void __afl_manual_init();
 static volatile char suppress_warning1 = AFL_DEFER_FORKSVR[0];
 
 // Input buffer.
-static const size_t kMaxAflInputSize = 1 << 20;
+static const size_t kMaxAflInputSize = 1024000;
 static uint8_t AflInputBuf[kMaxAflInputSize];
 
 // Keep track of where stderr content is being written to, so that
@@ -149,14 +129,6 @@ static void dup_and_close_stderr() {
   discard_output(output_fileno);
 }
 
-static void Printf(const char *Fmt, ...) {
-  va_list ap;
-  va_start(ap, Fmt);
-  vfprintf(output_file, Fmt, ap);
-  va_end(ap);
-  fflush(output_file);
-}
-
 // Close stdout and/or stderr if user asks for it.
 static void maybe_close_fd_mask() {
   char *fd_mask_str = getenv("AFL_DRIVER_CLOSE_FD_MASK");
@@ -171,57 +143,31 @@ static void maybe_close_fd_mask() {
 
 // Define LLVMFuzzerMutate to avoid link failures for targets that use it
 // with libFuzzer's LLVMFuzzerCustomMutator.
-extern "C" size_t LLVMFuzzerMutate(uint8_t *Data, size_t Size, size_t MaxSize) {
+size_t LLVMFuzzerMutate(uint8_t *Data, size_t Size, size_t MaxSize) {
   assert(false && "LLVMFuzzerMutate should not be called from afl_driver");
   return 0;
 }
 
-// Execute any files provided as parameters.
-static int ExecuteFilesOnyByOne(int argc, char **argv) {
-  for (int i = 1; i < argc; i++) {
-    std::ifstream in(argv[i], std::ios::binary);
-    in.seekg(0, in.end);
-    size_t length = in.tellg();
-    in.seekg(0, in.beg);
-    std::cout << "Reading " << length << " bytes from " << argv[i] << std::endl;
-    // Allocate exactly length bytes so that we reliably catch buffer overflows.
-    std::vector<char> bytes(length);
-    in.read(bytes.data(), bytes.size());
-    assert(in);
-    LLVMFuzzerTestOneInput(reinterpret_cast<const uint8_t *>(bytes.data()), bytes.size());
-    std::cout << "Execution successful" << std::endl;
-  }
-  return 0;
-}
-
 int main(int argc, char **argv) {
-  Printf("======================= INFO =========================\n"
+  printf("======================= INFO =========================\n"
          "This binary is built for a qemu_mode fuzzer.\n"
          "To run the target function on individual input(s) execute this:\n"
          "  %s < INPUT_FILE\n"
          "or\n"
-         "  %s INPUT_FILE1 [INPUT_FILE2 ... ]\n"
+         "  %s INPUT_FILE\n"
          "To fuzz with afl-fuzz execute this:\n"
-         "  afl-fuzz -Q [afl-flags] %s\n"
+         "  afl-fuzz [afl-flags] -- %s\n"
          "======================================================\n",
-         argv[0], argv[0], argv[0]);
+         argv[0], argv[0]);
 
   maybe_duplicate_stderr();
   maybe_close_fd_mask();
   if (LLVMFuzzerInitialize)
     LLVMFuzzerInitialize(&argc, &argv);
 
-  if (argc > 1 && argv[1][0] != '-' && atoi(argv[1]) <= 0)
-    return ExecuteFilesOnyByOne(argc, argv);
-
   ssize_t n_read = read(0, AflInputBuf, kMaxAflInputSize);
   if (n_read > 0) {
-    // Copy AflInputBuf into a separate buffer to let asan find buffer
-    // overflows. Don't use unique_ptr/etc to avoid extra dependencies.
-    uint8_t *copy = new uint8_t[n_read];
-    memcpy(copy, AflInputBuf, n_read);
-    LLVMFuzzerTestOneInput(copy, n_read);
-    //delete[] copy; // no need for cleanup
+    LLVMFuzzerTestOneInput(AflInputBuf, n_read);
   }
   return 0;
 }
